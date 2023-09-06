430dc83b9ed926daf79eaecb4576405eba1480e5
Co Quach <daico007@gmail.com>
2023-09-06 11:42:49 -0500

    modify setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,12 @@
-"""Shim for pyproject.toml files to have --editable installs."""
-import setuptools
+from setuptools import setup
 
-if __name__ == "__main__":
-    setuptools.setup()
+setup(
+    name="cecam_mosdef",
+    version="0.0.1",
+    descriptions="Materials for MoSDeF tutorial at the CECAM 2023 FAIR-MoSDeF workshop",
+    author="Co D. Quach, Nicholas C. Craven, Kieran Nehil-Puleo, Eric Jankowski",
+    author_email="daico007@gmail.com, nicholas.c.craven@vanderbilt.edu, kieran.d.nehil-puleo@vanderbilt.edu, ericjankowski@boisestate.edu",
+    license="MIT",
+    packages=["cecam_mosdef"],
+    zip_safe=False,
+)
